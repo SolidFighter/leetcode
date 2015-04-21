632547359794c7c9becee722c45c59c4a13033d1
myang <myang199088@163.com>
2015-04-21 21:48:33 +0800

    leetcode

--- Algorithms/remove_ele.py
+++ Algorithms/remove_ele.py
@@ -1,3 +1,9 @@
+# Remove Element
+#
+# Given an array and a value, remove all instances of that value in place and return the new length.
+#
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
 __author__ = 'myang'
 
 class Solution:
