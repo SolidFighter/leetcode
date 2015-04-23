c3ec4ef60c2cb10bcef7bc498e0a5ecb00c9171a
myang <myang199088@163.com>
2015-04-23 22:02:28 +0800

    leetcode

---
+++ Algorithms/pascal_triangle2.py
@@ -0,0 +1,21 @@
+__author__ = 'myang'
+
+class Solution:
+    # @param {integer} rowIndex
+    # @return {integer[]}
+    def getRow(self, rowIndex):
+        if rowIndex == 0:
+            return [1]
+        if rowIndex == 1:
+            return [1, 1]
+        pascal = [1, 1]
+        for i in range(1, rowIndex):
+            for j in range(0, i):
+                pascal[j] = pascal[j] + pascal[j+1]
+            pascal.insert(0, 1)
+        return pascal
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.getRow(2))
+
