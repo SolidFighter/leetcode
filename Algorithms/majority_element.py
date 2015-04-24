99065fcffabe8d02289652423703e77024ec0b81
myang <myang199088@163.com>
2015-04-24 22:20:37 +0800

    leetcode

--- Algorithms/majority_element.py
+++ Algorithms/majority_element.py
@@ -1,4 +1,4 @@
-# Majority Element Total Accepted: 34472 Total Submissions: 99657
+# Majority Element Total
 #
 # Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 #
